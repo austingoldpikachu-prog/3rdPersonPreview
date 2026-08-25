2f8be7dd4ed9546821de4c8da7a8397451c889a7
Bunghole <austin.goldpikachu@gmail.com>
2026-08-25 13:50:18 -0700

    add movement functions and camera behavior

--- Assets/Scripts/3rdPersonCam.cs
+++ Assets/Scripts/3rdPersonCam.cs
@@ -17,6 +17,16 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     public float rotationSpeed;
 
+    // public Transform combatLookAt;
+
+    // public CameraStyle currentStyle;
+
+    // public enum CameraStyle
+    // {
+    //     Basic,
+    //     Combat
+    // }
+
     public UnityEngine.Vector3 oldPos = default(UnityEngine.Vector3);
 
     private void Update()
@@ -26,14 +36,21 @@ public class NewMonoBehaviourScript : MonoBehaviour
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
+        // if(currentStyle == CameraStyle.Basic)
+        {
+            Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
+            Vector3 inputDir = orientation.forward *moveInput.y + orientation.right * moveInput.x;
+
+            if (inputDir != Vector3.zero)
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        // }
+        // else if(currentStyle == CameraStyle.Combat)
+        // {
+        //     Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+        // orientation.forward = dirToCombatLookAt.normalized;
+
+        //  playerObj.forward = dirToCombatLookAt.normalized;
+        }
         
-
-        Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
-        Vector3 inputDir = orientation.forward *moveInput.y + orientation.right * moveInput.x;
-
-        if (inputDir != Vector3.zero)
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
-
-
     }
 }
--- Assets/Scripts/Universal Movement.cs
+++ Assets/Scripts/Universal Movement.cs
@@ -5,55 +5,89 @@ using UnityEngine.InputSystem;
 public class UniversalMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
-
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
     public Transform orientation;
-
     float horizontalInput;
     float verticalInput;
-
-    public InputActionReference moveAction;
-
     private Vector3 inputDir;
     private Vector2 moveInput;
-
     public float groundDrag;
 
+    [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump = true;
-    public InputActionReference jumpAction;
-
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     private bool grounded;
-
     Vector3 moveDirection;
 
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    public float startYScale;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+
+    [Header("Input Action Refrences")]
+    public InputActionReference jumpAction;
+    public InputActionReference moveAction;
+    public InputActionReference sprintAction;
+    public InputActionReference crouchAction;
+
     Rigidbody rb;
+
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true; 
+        moveSpeed = walkSpeed;
+
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
     {
     grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
     rb.linearDamping = grounded ? groundDrag :0f;
+
+    // SlopeControl();
     }
 
     private void OnEnable()
     {
         jumpAction.action.performed += JumpCheck;
+        sprintAction.action.performed += SprintCheck;
+        sprintAction.action.canceled += SprintCheck;
+        crouchAction.action.performed += CrouchCheck;
+        crouchAction.action.canceled += CrouchCheck;
+
+
     }
 
     private void OnDisable()
     {
         jumpAction.action.performed -= JumpCheck;
+        sprintAction.action.performed -= SprintCheck;
+        sprintAction.action.canceled -= SprintCheck;
+        crouchAction.action.performed -= CrouchCheck;
+        crouchAction.action.canceled -= CrouchCheck;
+
     }
 
 
@@ -68,11 +102,31 @@ public class UniversalMovement : MonoBehaviour
     {
         inputDir = orientation.forward * moveInput.y + orientation.right * moveInput.x; 
 
+        // if(onSlope())
+        // {
+        //     rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+
+        //     if(rb.linearVelocity.y > 0)
+        //         rb.AddForce(Vector3.down *80f, ForceMode.Force);
+        // }
+
         if(grounded)
             rb.AddForce(inputDir.normalized * moveSpeed * 10f, ForceMode.Force);
         else
            rb.AddForce(inputDir.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        //    rb.useGravity = !onSlope();
     }
+
+    // private void SlopeControl()
+    // {
+    //     if(onSlope())
+    //     {
+    //         if(rb.linearVelocity.magnitude > moveSpeed)
+    //             rb.linearVelocity = rb.linearVelocity.normalized*moveSpeed; 
+    //     }
+
+    // }
 
     private void Jump()
     {
@@ -100,4 +154,53 @@ public class UniversalMovement : MonoBehaviour
             Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
+
+    private void SprintCheck(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if(grounded)
+                state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        else if (context.canceled)
+        {
+            state = MovementState.walking;
+        moveSpeed = walkSpeed;           
+        }
+
+    }
+
+    private void CrouchCheck(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3 .down * 5f, ForceMode.Impulse);
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+        else if(context.canceled)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+    }
+
+    // private bool onSlope()
+    // {
+    //     if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+    //     {
+    //         float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+    //         return angle < maxSlopeAngle && angle != 0;
+    //     }
+
+    //     return false;
+    // }
+
+    // private Vector3 GetSlopeMoveDirection()
+    // {
+    //     return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    // }
 }
